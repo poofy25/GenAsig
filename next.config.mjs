d84a497cd76be53cc2723461e8765785f1f9e563
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-02-16 09:42:06 +0200

    configs

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,10 @@
         },
         env: {
           BASE_URL: process.env.BASE_URL,
-        }
+        },
+        eslint: {
+          ignoreDuringBuilds: true,
+        },
       }
 
 export default nextConfig;
